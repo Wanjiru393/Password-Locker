a5dc0ed09029736c32ba6f37ee0e59f2d43cab59
Wanjiru393 <carolciru393@gmail.com>
2022-04-24 13:41:36 +0300

    Added fuction to save new user instance

--- user.py
+++ user.py
@@ -7,4 +7,8 @@ def _init_(self,user_name,password):
     self.user_name = user_name
     self.password = password
 
-    
+def save_user(self):
+    """
+    Method to save new user instance to the user_list
+    """
+    User.user_list.append(self)
