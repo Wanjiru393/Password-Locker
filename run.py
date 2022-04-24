03737e5c98640dd02043325f35bb6f5704ec4843
Wanjiru393 <carolciru393@gmail.com>
2022-04-24 14:00:31 +0300

    Added navigation shortcode

--- run.py
+++ run.py
@@ -1,2 +1,28 @@
 import random
 from user import User
+
+
+def main():
+
+    while True:
+        print("Welcome!")
+        print('\n')
+        print("Select a short code to navigate through:'nu' create new user,'lg' Login to existing account,'ex' exit.")
+        short_code = input().lower()
+        print('\n')
+
+        if short_code =='nu':
+            print('Create your username')
+            created_user_name =input()
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
