8ce1c0737619b8ee88d128b14ef9c33c6c4d4237
Wanjiru393 <carolciru393@gmail.com>
2022-04-24 18:58:03 +0300

    called functions for lg shortcode

--- run.py
+++ run.py
@@ -37,6 +37,45 @@ def main():
                 print('Enter your password')
                 entered_password = input()
 
+            while entered_username !=created_user_name or entered_password!=created_user_password:
+                print("Invalid username or password")
+                print("Username")
+                entered_username = input()
+                print("Enter your pasword")
+                entered_password = input()
+
+            else:
+                print(f"{entered_username},welcome to your account")
+                print('\n')
+
+
+        elif short_code =='lg':
+            print("Welcome")
+            print('Enter username')
+            default_user_name =input()
+
+            print('Enter your Password')
+            default_user_password =input()
+            print('\n')
+            while default_user_name != 'testUser' or default_user_password != '09876':
+                print("Wrong username or password.username 'testuser' and password '09876'")
+                print("Enter user name")
+                default_user_name =input()
+
+                print("Enter password")
+                default_user_password = input()
+                print('\n')
+
+            else:
+                print("Login success")
+                print('\n')
+                print('\n')
+
+        
+
+
+            
+
 
 
 
