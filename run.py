5e35c830721f838369866825daf57f10c498b69a
Wanjiru393 <carolciru393@gmail.com>
2022-04-24 14:13:44 +0300

    Added username & Password validation

--- run.py
+++ run.py
@@ -15,6 +15,29 @@ def main():
             print('Create your username')
             created_user_name =input()
 
+            print('create password')
+            created_user_password =input()
+
+            print('confirm password')
+            confirm_password =input()
+
+            while confirm_password !=created_user_password:
+                print('Invalid! Password does not match')
+                print('Enter your password')
+                created_user_password = input()
+                print('confirm password')
+                confirm_password = input()
+
+            else:
+                print(f"{created_user_name}, Account created successfully!")
+                print('\n')
+                print('proceed to login')
+                print('Username:')
+                entered_username =input()
+                print('Enter your password')
+                entered_password = input()
+
+
 
 
 
