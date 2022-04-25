443668c797d168a4fc8659be0f080ec770607254
Wanjiru393 <carolciru393@gmail.com>
2022-04-25 08:17:13 +0300

    Added delete & display funtions

--- user.py
+++ user.py
@@ -12,3 +12,14 @@ def save_user(self):
     Method to save new user instance to the user_list
     """
     User.user_list.append(self)
+
+
+@classmethod
+def display_user(cls):
+    return cls.user_list
+
+def delete_user(self):
+    '''
+    delete_account method deletes a  saved account from the list
+    '''
+    User.user_list.remove(self)
